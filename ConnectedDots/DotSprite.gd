7c46222bfb545b3311fdec375020a4e2325171b6
Jay Ess <33491863+Spearfield@users.noreply.github.com>
2020-09-21 00:55:57 +0200

    Cleanup.

---
+++ ConnectedDots/DotSprite.gd
@@ -0,0 +1,7 @@
+extends Sprite
+
+var direction : Vector2
+var speed : float
+
+func _ready():
+	pass # Replace with function body.
--- ConnectedDots/Node2D.gd
+++ ConnectedDots/Node2D.gd
@@ -1,40 +1,38 @@
 extends Node2D
 
-const NumberOfPoints = 20
+const NumberOfPoints = 30
 const LineRange = 150
+const lineWidth = 2
+const antiAliasLine = true
+
 var rng = RandomNumberGenerator.new()
-var points = []
-var directions = []
-var speed = []
-var screenSizeX
-var screenSizeY
+var points: Array = []
+var screenSize : Vector2
 var lineColor = Color(1,1,1)
 onready var Dot = preload("res://DotSprite.tscn")
 
 func _ready():
-	screenSizeX = get_viewport().size.x
-	screenSizeY = get_viewport().size.y
+	screenSize = get_viewport().size
 	rng.randomize()
 	for i in range(NumberOfPoints):
 		var dot = Dot.instance()
-		dot.position.x = rng.randf_range(0,screenSizeX)
-		dot.position.y = rng.randf_range(0,screenSizeY)
+		dot.position.x = rng.randf_range(0,screenSize.x)
+		dot.position.y = rng.randf_range(0,screenSize.y)
+		dot.direction = Vector2(rng.randf_range(-1,1), rng.randf_range(-1,1))
+		dot.speed = rng.randf_range(0.2,1)
 		points.append(dot)
 		add_child(dot)
-		directions.append(Vector2(rng.randf_range(-1,1),
-															rng.randf_range(-1,1)))
-		speed.append(rng.randf_range(0.2,1)) # I do know that I could have direction * speed in one array...
 
 func _draw():
 	for i in range(NumberOfPoints):
-		points[i].position += directions[i] * speed[i]
-		if points[i].position.x > screenSizeX:	points[i].position.x = 0
-		if points[i].position.y > screenSizeY:	points[i].position.y = 0
-		if points[i].position.x < 0:						points[i].position.x = screenSizeX
-		if points[i].position.y < 0:						points[i].position.y = screenSizeY
+		points[i].position += points[i].direction * points[i].speed
+		if points[i].position.x > screenSize.x:	points[i].position.x = 0
+		if points[i].position.y > screenSize.y:	points[i].position.y = 0
+		if points[i].position.x < 0:						points[i].position.x = screenSize.x
+		if points[i].position.y < 0:						points[i].position.y = screenSize.y
 		for j in range(NumberOfPoints):
 			if points[i].position.distance_to(points[j].position) <= LineRange:
-				draw_line( points[i].position, points[j].position, lineColor, 2 , true )
+				draw_line( points[i].position, points[j].position, lineColor, lineWidth , antiAliasLine )
 
 func _process(delta):
 	update()
